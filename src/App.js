d1f4d3cc2105f09387752fe17c7ddef4bf325ff1
kaan-scope <kaan.vurgun@scope.digital>
2021-01-13 19:21:53 +0300

    Rev

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Navigation, Footer, Home, About } from "./components";
+import { Navigation, Footer, Home, Detay } from "./components";
 function App() {
   return (
     <div className="App">
